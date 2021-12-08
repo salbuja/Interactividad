f73da88f8318f1ac57b0ac05b8489add87c13f5b
Santiago Albuja <salbuja@cotopaxi.k12.ec>
2021-12-07 19:41:44 -0500

    Cuarta Version Funcional slider arreglado

--- src/app/shared/actuales.ts
+++ src/app/shared/actuales.ts
@@ -26,7 +26,7 @@ export const ACTUALES: Actual[] = [
         id: '2',
         name: 'Naomi Osaka',
         image: 'assets/images/Naomi Osaka.png',
-        description: 'Hija de un padre haitiano y una madre japonesa, Osaka lleva entrenando casi toda su vida. Es Actualmente reconocida como la Jugadora numero 1 por la asociación de tenis femenil, es una 4 veces ganadora del Gran Slam individual y la campeona actual del abierto de Australia.',
+        description: 'Hija de un padre haitiano y una madre japonesa, Osaka lleva entrenando casi toda su vida. Es reconocida por ser la primera Jugadora asiática en llegar al número 1 de la asociación de tenis femenil WTA, es una 4 veces ganadora del Gran Slam individual y la campeona actual del abierto de Australia.',
         audios: [{
             title: 'Naomi Osaka',
             link: 'assets/audios/Naomi Osaka.mp3'
